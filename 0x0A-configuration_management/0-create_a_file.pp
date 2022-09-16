7436d388d13ed47e0bf921e61e4ecd251f9ada20
Nahom.Gulte <nahomg@awash.local>
2022-09-16 15:37:50 +0300

    commit for configuration managment project

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,10 @@
 # Create a file in /tmp
 
-  file { 'school':
-    ensure  => 'present',
-    content => 'I love puppet',
-    group   =>'www-data',
-    owner   =>'www-data',
-    mode    => '0744',
-    path    => '/tmp/school',
-  }
+file { 'school':
+  ensure  => 'present',
+  content => 'I love Puppet',
+  group   => 'www-data',
+  mode    => '0744',
+  owner   => 'www-data',
+  path    => '/tmp/school',
+}
